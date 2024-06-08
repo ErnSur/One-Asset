06adcd95c8071f267821c8ba727499ea39cbf250
Ernest Suryś <ernest.surys@gmail.com>
2024-06-08 13:57:27 +0200

    virtual interface

--- Packages/com.quickeye.one-asset/Samples/EventSystem/Runtime/GameEvent.cs
+++ Packages/com.quickeye.one-asset/Samples/EventSystem/Runtime/GameEvent.cs
@@ -3,46 +3,46 @@ using UnityEngine.Events;
 
 namespace QuickEye.EventSystem
 {
+    using System;
+
     // TODO:
     // Generate Event asset as a GameEvents.asset sub-asset
-        // assets should be regenerated on recompile
-        // This would prevent old unused assets from piling up. Con: This would also delete assets that may still be referenced somewhere...
-        // TODO: how to handle event renames?
-        // This would force correct names for all of the events at all times
-        // easier to find events in the project
+    // assets should be regenerated on recompile
+    // This would prevent old unused assets from piling up. Con: This would also delete assets that may still be referenced somewhere...
+    // TODO: how to handle event renames?
+    // This would force correct names for all of the events at all times
+    // easier to find events in the project
     // Every Game Event should have it's own unique ID different from the name so that renames don't break the references
     public abstract class GameEvent<TArgs> : GameEventBase, IInvokable
     {
         [SerializeField]
         TArgs _lastPayload;
 
-        [SerializeField]
-        [HideInInspector]
-        UnityEvent<TArgs> _event = new UnityEvent<TArgs>();
+        [field: NonSerialized]
+        public UnityEvent<TArgs> Event { get; protected set; } = new UnityEvent<TArgs>();
 
-        public UnityEvent<TArgs> Event => _event;
         public TArgs LastPayload => _lastPayload;
 
-        public void Subscribe(UnityAction<TArgs> callback)
+        public virtual void Subscribe(UnityAction<TArgs> callback)
         {
             Event.AddListener(callback);
         }
 
-        public void Unsubscribe(UnityAction<TArgs> callback)
+        public virtual void Unsubscribe(UnityAction<TArgs> callback)
         {
             Event.RemoveListener(callback);
         }
 
-        public void Invoke(TArgs payload)
+        public virtual void Invoke(TArgs payload)
         {
             Debug.Log($"[!] Invoke: {name} with: {payload}");
             Event?.Invoke(_lastPayload = payload);
             wasInvoked = true;
         }
+
         protected override void ResetValues()
         {
             base.ResetValues();
-            _event = new UnityEvent<TArgs>();
             _lastPayload = default;
         }
 
